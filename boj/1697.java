99ec83a0e1506f9e84df0e0d37fa32face69e57b
심지원 <shark.cametotea@gmail.com>
2019-03-18 11:48:58 +0900

    modify 1697.java

--- boj/1697.java
+++ boj/1697.java
@@ -4,39 +4,41 @@ import java.util.Scanner;
 
 public class Main {
     static int N, K;
-    static int visited[] = new int[100002];
+    static int depth[];
 
     public static void bfs() {
         Queue<Integer> q = new LinkedList<>();
 
-        q.add(N);
-        visited[N] = 1;
+        q.offer(N);
+        depth[N] = 1;
         while (!q.isEmpty()) {
             N = q.poll();
 
             if (N == K) {
                 break;
             }
-            if (N-1 >= 0 && visited[N - 1] == 0) {
-                q.add(N - 1);
-                visited[N - 1] = visited[N] + 1;
+            if (N + 1 <= 100000 && depth[N + 1] == 0) {
+                q.offer(N+1);
+                depth[N+1] = depth[N] + 1;
             }
-            if (N <= 100000 && visited[N + 1] == 0) {
-                q.add(N + 1);
-                visited[N + 1] = visited[N] + 1;
+            if (N * 2 <= 100000 && depth[N * 2] == 0) {
+                q.offer(N*2);
+                depth[N*2] = depth[N] + 1;
             }
-            if (N * 2 <= 100000 && visited[N * 2] == 0) {
-                q.add(N * 2);
-                visited[N * 2] = visited[N] + 1;
+            if (N - 1 >= 0 && depth[N-1] == 0) {
+                q.offer(N-1);
+                depth[N-1] = depth[N] + 1;
             }
         }
-        System.out.println(visited[K]-1);
+        System.out.println(depth[K]-1);
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         N = sc.nextInt();
         K = sc.nextInt();
+        depth = new int[100001];
 
         bfs();
     }
