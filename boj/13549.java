6bb43150ce4ac6dd4c40f333da8dea0a2149e1de
심지원 <shark.cametotea@gmail.com>
2019-03-20 21:14:04 +0900

    modify 13549

--- boj/13549.java
+++ boj/13549.java
@@ -3,8 +3,9 @@ import java.util.Queue;
 import java.util.Scanner;
 
 class Pair {
-    int current = 0, count = 0;
-    Pair(int current, int count) {
+    int current = 0;
+    int count = 0;
+    public Pair(int current, int count) {
         this.current = current;
         this.count = count;
     }
@@ -13,21 +14,26 @@ class Pair {
 public class Main {
     static int N, K;
     static boolean visited[];
-    static int min = Integer.MAX_VALUE;
 
     public static void bfs() {
         Queue<Pair> q = new LinkedList<>();
+
         q.offer(new Pair(N, 0));
         while (!q.isEmpty()) {
             Pair p = q.poll();
+
             if (p.current == K) {
-                min = Math.min(min, p.count);
+                System.out.println(p.count);
                 break;
             }
+
             if (visited[p.current]) continue;
             else visited[p.current] = true;
 
-            // 틀렸던 이유 : depth[]로 해서.. visited[]로 *2를 먼저 체크해줘야함.
+            //내가 틀린 이유 1. visited[]가 아닌 depth[]를 써서.
+            //class Pair로 해주는 게 더 가독성이 좋고 안 헷갈리니까 담부터는 Pair 이용!
+            //내가 틀린 이유 2. if의 순서. *2를 먼저 넣고 -1인 경우도 +1보다 우선으로 넣어줘야 한다.
+            //왜냐하면... (모름)
             if (p.current * 2 <= 100000 && !visited[p.current * 2]) {
                 q.offer(new Pair(p.current * 2, p.count));
             }
@@ -46,6 +52,5 @@ public class Main {
         K = sc.nextInt();
         visited = new boolean[100001];
         bfs();
-        System.out.println(min);
     }
 }
