262b44f5d4b6f64fd812401bfbb8904ae3ed2aab
심지원 <shark.cametotea@gmail.com>
2019-04-02 15:10:06 +0900

    다음 순열

--- boj/10972.java
+++ boj/10972.java
@@ -3,50 +3,50 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-//Gert Taberner - Fallen
-
 public class Main {
-    public static boolean brute_force(int arr[]) {
-        boolean flag = false;
-        int i = 0, j = 0;
-        for (i = arr.length - 1; i > 0; i--) {
-            for (j = i - 1; j >= 0; j--) {
-                if (arr[i] > arr[j]) {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                    flag = true;
-                    break;
-                }
-            }
-            if (flag) break;
+    static int arr[];
+
+    public static void swap(int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static boolean next() {
+        int index = -1;
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] < arr[i+1]) index = i;
         }
 
-        if (flag) {
-            for (int k = j + 1; k < arr.length-1; k++) {
-                if (arr[k] < arr[k+1]) continue;
-                else {
-                    int temp = arr[k];
-                    arr[k] = arr[k+1];
-                    arr[k+1] = temp;
-                }
-            }
-            return true;
+        if (index == -1) return false;
+
+        int j = arr.length - 1;
+        for (; j >= 0; j--) {
+            if (arr[j] > arr[index]) break;
         }
-        return false;
+
+        // swap
+        swap(index, j);
+
+        for (int i = index + 1; i < (arr.length + index + 1) / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - (i - index)];
+            arr[arr.length - (i - index)] = temp;
+        }
+        return true;
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(br.readLine());
 
-        int arr[] = new int[N];
-        st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        arr = new int[N];
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        if (brute_force(arr)) {
+
+        if (next()) {
             for (int i = 0; i < arr.length; i++) {
                 System.out.print(arr[i] + " ");
             }
