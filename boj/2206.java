dcfb9cd6bfc853fa37d885398c3600200d581e2f
심지원 <shark.cametotea@gmail.com>
2019-04-11 22:00:32 +0900

    벽 부수고 이동하기(실패)

--- boj/2206.java
+++ boj/2206.java
@@ -5,8 +5,13 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+// 벽 부수고 이동하기 FAQ https://www.acmicpc.net/board/view/27386
+
 class Pair {
-    int x, y, broken;
+    int x;
+    int y;
+    int broken;
+
     Pair(int x, int y, int broken) {
         this.x = x;
         this.y = y;
@@ -15,74 +20,68 @@ class Pair {
 }
 
 public class Main {
-    static int N, M, arr[][];
-    static boolean isBroken = false, visited[][][];
-    static int to_x[] = {0, 0, 1, -1};
-    static int to_y[] = {1, -1, 0, 0};
+    static int N, M, arr[][], count;
+    static int toX[] = {0, 0, 1, -1};
+    static int toY[] = {1, -1, 0, 0};
+    static boolean visited[][][];
+    static int WALL = 1, EMPTY = 0;
 
-    public static boolean check_ragne(int x, int y) {
+    public static boolean checkRange(int x, int y) {
         if (x >= 0 && x < N && y >= 0 && y < M) return true;
         else return false;
     }
 
     public static int bfs() {
+        boolean end = false;
+
         Queue<Pair> q = new LinkedList<>();
+        q.offer(new Pair(0, 0, EMPTY));
+        visited[0][0][WALL] = true;
+        visited[0][0][EMPTY] = true;
 
-        q.offer(new Pair(0, 0, 0));
-        visited[0][0][0] = true;
-        visited[0][0][1] = true;
-
-        int count = 0;
-        boolean move = true;
-        while (!q.isEmpty() && !isBroken) {
+        while (!q.isEmpty()) {
             count++;
-            System.out.println("count++");
+            int size = q.size();
 
-            for (int j = 0; j < q.size(); j++) {
+            for (int j = 0; j < size; j++) {
                 Pair p = q.poll();
-//            System.out.println(p.broken);
-                System.out.println("current : (" + p.x + ", " + p.y + ")");
+
 
                 if (p.x == N - 1 && p.y == M - 1) {
-                    isBroken = true;
+                    end = true;
                     break;
                 }
 
-                for (int i = 0; i < 4; i++) {
-                    int go_x = p.x + to_x[i];
-                    int go_y = p.y + to_y[i];
 
-                    if (!check_ragne(go_x, go_y)) continue;
+                for (int i = 0; i < 4; i++) {
+                    int goX = p.x + toX[i];
+                    int goY = p.y + toY[i];
+
+                    if (!checkRange(goX, goY)) continue;
 
                     //벽인 경우
-                    if (arr[go_x][go_y] == 1) {
-                        //p.broken == 0이어야 함.
-                        if (p.broken == 0 && !visited[go_x][go_y][1]) {
-                            System.out.println("(" + p.x + ", " + p.y + ") -> (" + go_x + ", " + go_y + ")");
-                            q.offer(new Pair(go_x, go_y, 1));
-                            visited[go_x][go_y][1] = true;
+                    if (arr[goX][goY] == 1) {
+                        if (!visited[goX][goY][WALL] && p.broken == EMPTY) {
+                            q.offer(new Pair(goX, goY, WALL));
+                            visited[goX][goY][WALL] = true;
                         }
                     }
                     //벽이 아닌 경우
                     else {
-                        if (!visited[go_x][go_y][p.broken]) {
-                            System.out.println("(" + p.x + ", " + p.y + ") -> (" + go_x + ", " + go_y + ")");
-                            q.offer(new Pair(go_x, go_y, p.broken));
-                            visited[go_x][go_y][p.broken] = true;
+                        if (!visited[goX][goY][p.broken]) {
+                            q.offer(new Pair(goX, goY, p.broken));
+                            visited[goX][goY][p.broken] = true;
                         }
                     }
                 }
-                System.out.println("out");
             }
-            System.out.println("out2");
         }
-        return isBroken ? count : -1;
+        return end ? count : -1;
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
