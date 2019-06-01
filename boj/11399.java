96c81f4317b9210592ea17ed0278f411ce93a7a1
심지원 <shark.cametotea@gmail.com>
2019-06-01 15:20:31 +0900

    ATM

---
+++ boj/11399.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int N = s.nextInt();
+        int arr[] = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = s.nextInt();
+        }
+
+        Arrays.sort(arr);
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j <= i; j++) {
+                sum += arr[j];
+            }
+        }
+
+        System.out.printf("%d", sum);
+
+        /*
+        i : 2 5 1 4 3
+        t : 1 2 3 3 4
+
+        1 = 1
+        1 + 2 = 3
+        1 + 2 + 3 = 6
+        1 + 2 + 3 + 3 = 9
+        1 + 2 + 3 + 3 + 4 = 13
+         */
+    }
+}
