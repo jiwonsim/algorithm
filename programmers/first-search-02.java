a83683c8f276148a1cea5087de3b3c2690e66278
심지원 <shark.cametotea@gmail.com>
2019-06-03 21:09:25 +0900

    네트워크

--- programmers/first-search-02.java
+++ programmers/first-search-02.java
@@ -1,26 +1,43 @@
-class Solution {
-    boolean visited[][] ;
-    void dfs(int index, int n, int computers[][]) {
-        for (int i = 0; i < n; i++) {
-            if (visited[index][i]) continue;
-            if (computers[index][i] == 0) continue;
-            visited[index][i] = true;
-            visited[i][index] = true;
-            dfs(i, n, computers);
-        }
-    }
+import java.util.*;
 
+class Solution {
     public int solution(int n, int[][] computers) {
         int answer = 0;
 
-        visited = new boolean[n][n];
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            if (visited[i][i]) continue;
-            dfs(i, n, computers);
-            answer++;
+            list.add(new ArrayList<Integer>());
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (computers[i][j] == 1) {
+                    list.get(i).add(j);
+                }
+            }
         }
 
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] visited = new boolean[n];
+        for (int i = 0; i < list.size(); i++) {
+            if (!visited[i]) {
+                q.add(i);
+                visited[i] = true;
+                answer++;
+            }
 
+            while (!q.isEmpty()) {
+                int cur = q.poll();
+
+                for (int j = 0; j < list.get(cur).size(); j++) {
+                    int next = list.get(cur).get(j);
+                    if (visited[next]) {
+                        continue;
+                    }
+                    visited[next] = true;
+                    q.add(next);
+                }
+            }
+        }
         return answer;
     }
 }
