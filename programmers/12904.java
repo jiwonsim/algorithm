c25835c96b279fd2b87066bb3e58846533d79fb7
심지원 <shark.cametotea@gmail.com>
2019-06-25 16:08:23 +0900

    가장 긴 팰린드롬

--- programmers/12904.java
+++ programmers/12904.java
@@ -1,31 +1,31 @@
 
 class Solution
 {
-    public int solution(String str)
+    public int solution(String s)
     {
-        boolean[][] table = new boolean[2500][2500];
-        int len = 0;
+        int answer = 1;
 
-        for (int i = 0; i < str.length(); i++) table[i][i] = true;
+        boolean[][] arr = new boolean[2500][2500];
+        for (int i = 0; i < s.length(); i++) arr[i][i] = true;
 
-        for (int i = 0; i < str.length() - 1; i++) {
-            if (str.charAt(i) == str.charAt(i + 1)) {
-                table[i][i + 1] = true;
-                len = 2;
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                arr[i][i + 1] = true;
+                answer = 2;
             }
         }
 
-        int k;
-        for (int i = 3; i <= str.length(); i++) {
-            for (int j = 0; j <= str.length() - i; j++) {
-                k = i + j - 1;
-                if (str.charAt(j) == str.charAt(k)) {
-                    if (!table[j + 1][k - 1]) continue;
-                    table[j][k] = true;
-                    if (i > len) len = i;
+        for (int i = 3; i <= s.length(); i++) {
+            for (int j = 0; j <= (s.length() - i); j++) {
+                int k = i + j - 1; // 끝 인덱스
+                if (s.charAt(j) == s.charAt(k)) {
+                    if (!arr[j + 1][k - 1]) continue;
+                    arr[j][k] = true;
+                    if (i > answer) answer = i;
                 }
             }
         }
-        return len;
+
+        return answer;
     }
 }
