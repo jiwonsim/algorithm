e38ef111ec052192c07593fc0ea739cbe9c25767
심지원 <shark.cametotea@gmail.com>
2019-07-11 13:28:31 +0900

    숫자 카드

---
+++ boj/10815.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        HashSet<Integer> A = new HashSet<>();
+        for (int i = 0; i < N; i++) A.add(sc.nextInt());
+
+        int M = sc.nextInt();
+        int[] B = new int[M + 1];
+        int[] res = new int[M];
+        for (int i = 0; i < M; i++) {
+            B[i] = sc.nextInt();
+            if (A.contains(B[i])) res[i] = 1;
+        }
+
+        for (int i = 0; i < M; i++) System.out.printf("%d ", res[i]);
+    }
+}
+
+// 339808KB    4308ms
