0699e01fc1b37f3b561920499ffbc5af0269e96a
심지원 <shark.cametotea@gmail.com>
2019-10-18 20:58:00 +0900

    트리 순회

--- boj/1991.java
+++ boj/1991.java
@@ -1,76 +1,55 @@
 import java.util.*;
 
 public class Main {
-    static int N;
-    static HashMap<String, ArrayList<String>> hm;
-    static boolean[] visited;
-    public static void preorder(String start) {
-        System.out.printf("%s", start);
-        visited[start.charAt(0) - 'A'] = true;
 
-        for (String val : hm.get(start)) {
-            if (val.equals(".")) continue;
-            visited[start.charAt(0) - 'A'] = true;
-            preorder(val);
-            visited[start.charAt(0) - 'A'] = false;
-        }
+    static void preorder(int[][] arr, int root) {
+        if (root == -1) return;
+
+        System.out.printf("%c", root + 'A');
+        preorder(arr, arr[root][0]);
+        preorder(arr, arr[root][1]);
     }
 
-    public static void inorder(String start) {
-        if (visited[start.charAt(0) - 'A']) return;
-        String before = "";
+    static void inorder(int[][] arr, int root) {
+        if (root == -1) return;
 
-        for (String val : hm.get(start)) {
-            if (!val.equals(".")) {
-                before = val;
-                continue;
-            }
-            System.out.printf("index : %d \n", before.charAt(0) - 'A');
-            visited[before.charAt(0) - 'A'] = true;
-            inorder(before);
-            visited[before.charAt(0) - 'A'] = false;
-        }
+        inorder(arr, arr[root][0]);
+        System.out.printf("%c", root + 'A');
+        inorder(arr, arr[root][1]);
     }
 
-    public static void postorder(String start) {
+    static void postorder(int[][] arr, int root) {
+        if (root == -1) return;
 
+        postorder(arr, arr[root][0]);
+        postorder(arr, arr[root][1]);
+        System.out.printf("%c", root + 'A');
     }
-
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        hm = new HashMap<>();
-        visited = new boolean[N + 1];
-        ArrayList<String> val;
-        String key;
+        int[][] arr = new int[26][2];
+
+        int N = sc.nextInt();
+
+        sc.nextLine();
         for (int i = 0; i < N; i++) {
-            key = sc.next();
-            if (hm.get(key) == null) {
-                val = new ArrayList<>();
-            }
-            else {
-                val = hm.get(key);
-            }
-            for (int j = 0; j < 2; j++) {
-                val.add(sc.next());
-            }
-            hm.put(key, val);
+
+            String in = sc.nextLine();
+
+            int root = in.charAt(0) - 'A';
+
+            if (in.charAt(2) == '.') arr[root][0] = -1;
+            else arr[root][0] = in.charAt(2) - 'A';
+
+            if (in.charAt(4) == '.') arr[root][1] = -1;
+            else arr[root][1] = in.charAt(4) - 'A';
         }
 
-//        for (String k : hm.keySet()) {
-//            System.out.printf("%s ", k);
-//            for (String v : hm.get(k))
-//                System.out.printf("%s ", v);
-//            System.out.printf("\n");
-//        }
-
-        preorder("A");
+        preorder(arr, 0);
         System.out.printf("\n");
-        for (String k : hm.keySet()) {
-            inorder(k);
-        }
-//        Arrays.fill(visited, false);
-//        postorder("A");
+        inorder(arr, 0);
+        System.out.printf("\n");
+        postorder(arr, 0);
     }
 }
