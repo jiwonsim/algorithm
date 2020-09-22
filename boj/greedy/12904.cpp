b7ddea5d1d152a26dc8d7b910bd6d2602274f53a
심지원 <shark.cametotea@gmail.com>
2020-09-22 11:39:39 +0900

    BOJ - A와 B(그리디)

---
+++ boj/greedy/12904.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+
+    string s, t;
+    
+    cin >> s >> t;
+
+    while (t.length() > s.length()) {
+        if (t.back() == 'A') t.pop_back();
+        else {
+            t.pop_back();
+            reverse(t.begin(), t.end());
+        }
+    }
+
+    if (s==t) cout << 1 << '\n';
+    else cout << 0 << '\n';
+
+    return 0;
+}
